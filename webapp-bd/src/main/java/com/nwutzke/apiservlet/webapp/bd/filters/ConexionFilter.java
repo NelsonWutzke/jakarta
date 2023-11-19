aeaf481f4fa3d4ed6e84a61e27f4aad786769f6b
Nelson Wutzke <wutzkenelson@gmail.com>
2023-11-19 12:07:43 -0300

    Pool de Conexiones JDBC con TomcatDatasource

--- webapp-bd/src/main/java/com/nwutzke/apiservlet/webapp/bd/filters/ConexionFilter.java
+++ webapp-bd/src/main/java/com/nwutzke/apiservlet/webapp/bd/filters/ConexionFilter.java
@@ -1,11 +1,12 @@
 package com.nwutzke.apiservlet.webapp.bd.filters;
 
 import com.nwutzke.apiservlet.webapp.bd.services.ServiceJdbcException;
-import com.nwutzke.apiservlet.webapp.bd.util.ConexionBaseDatos;
+import com.nwutzke.apiservlet.webapp.bd.util.ConexionBaseDatosDS;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.naming.NamingException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -15,7 +16,7 @@ public class ConexionFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        try (Connection conn = ConexionBaseDatos.getConnection()) {
+        try (Connection conn = ConexionBaseDatosDS.getConnection()) {
             if(conn.getAutoCommit())
                 conn.setAutoCommit(false);
 
@@ -30,7 +31,7 @@ public class ConexionFilter implements Filter {
                 ((HttpServletResponse)servletResponse).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,e.getMessage());
                 e.printStackTrace();
             }
-        } catch (SQLException e) {
+        } catch (SQLException | NamingException e) {
             e.printStackTrace();
         }
     }
