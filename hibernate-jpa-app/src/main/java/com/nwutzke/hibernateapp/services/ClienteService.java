707e94d16dffe1456cc012098b8de753dfe610bb
Nelson Wutzke <wutzkenelson@gmail.com>
2023-12-04 14:39:17 -0300

    Crud service

---
+++ hibernate-jpa-app/src/main/java/com/nwutzke/hibernateapp/services/ClienteService.java
@@ -0,0 +1,14 @@
+package com.nwutzke.hibernateapp.services;
+
+import com.nwutzke.hibernateapp.entity.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteService {
+
+    List<Cliente> listar();
+    Optional<Cliente> porId(Long id);
+    void guardar(Cliente cliente);
+    void eliminar(Long id);
+}
